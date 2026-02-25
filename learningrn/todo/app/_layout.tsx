ccef57da24cfd16e1b1627e29fa58d8e974c80fe
gopimothukuri <gopimothukuri26@gmail.com>
2026-02-25 23:35:58 +0530

    Refactor Convex client initialization with Constants

--- learningrn/todo/app/_layout.tsx
+++ learningrn/todo/app/_layout.tsx
@@ -1,26 +1,22 @@
-
 import { Stack } from "expo-router";
 import { ThemeProvider } from "@/hooks/useTheme";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import Constants from "expo-constants";
 
-const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+const convexUrl = Constants.expoConfig?.extra?.convexUrl;
+
+const convex = new ConvexReactClient(convexUrl!, {
   unsavedChangesWarning: false,
 });
 
-
-
 export default function RootLayout() {
   return (
-        <ConvexProvider client={convex}>
-
-    <ThemeProvider>
-        <Stack screenOptions={{ headerShown: false }} 
-  >
-    <Stack.Screen name="(tab)"/>
-  </Stack>
-    </ThemeProvider>
-        </ConvexProvider>
-
-
+    <ConvexProvider client={convex}>
+      <ThemeProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tab)" />
+        </Stack>
+      </ThemeProvider>
+    </ConvexProvider>
   );
 }
